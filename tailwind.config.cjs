667bcf1945f462f9729094d46932334d7819bf10
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-09-13 07:16:19 -0300

    :wrench: setup font-family `Inter` in project
    add configuration in index.html and tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,8 +10,11 @@ module.exports = {
         galaxy: "url('/background-galaxy.png')",
         'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
         'nlw-gradient': 'linear-gradient(90deg, #9572FC 0%, #43E7AD 50.00%, #E1D55D 100%)',
-      }
+      },
     },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    }
   },
   plugins: [],
 }
